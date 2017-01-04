f9826a7de84c1ab122d6fcf5f7b2e538f7872eae
297739 <297739@SWE-297739-A>
2017-01-04 06:08:39 -0800

    Adding subtask item and template

---
+++ GRiD Subtask Editor/SubtaskItem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRiD_Subtask_Editor
+{
+    [Serializable()]
+    public class SubtaskItem
+    {
+
+        bool? addSubtask;
+        string summary;
+        string description;
+        string assignee;
+        double estimate;
+
+        public bool? AddSubtask
+        {
+            get { return addSubtask; }
+            set { addSubtask = value; }
+        }
+        public string Summary
+        {
+            get { return summary; }
+            set { summary = value; }
+        }
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        public string Assignee
+        {
+            get { return assignee; }
+            set { assignee = value; }
+        }
+        public double Estimate
+        {
+            get { return estimate; }
+            set { estimate = value; }
+        }
+    }
+}
---
+++ GRiD Subtask Editor/SubtaskTemplate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRiD_Subtask_Editor
+{
+    [Serializable]
+    class SubtaskTemplate
+    {
+        private List<SubtaskItem> template;
+
+        public List<SubtaskItem> Template
+        {
+            get
+            {
+                return template;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return template.Count;
+            }
+        }
+
+        public SubtaskTemplate()
+        {
+            template = new List<SubtaskItem>();
+        }
+
+        public void AddSubtaskItem(SubtaskItem item)
+        {
+            template.Add(item);
+        }
+
+    }
+}
